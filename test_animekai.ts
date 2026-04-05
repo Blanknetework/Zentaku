0b42c674951c421dca87bec51f3f07ba74accc03
Adam Nuevo <adamnuevo66@gmail.com>
2026-04-05 22:29:21 +0800

    fixing some bugs

--- test_animekai.ts
+++ test_animekai.ts
@@ -1,12 +1,21 @@
-import { HiAnime } from "hianime";
+import { ANIME } from "@consumet/extensions";
 
 async function main() {
-  const hianime = new HiAnime.Scraper();
+  const animekai = new ANIME.AnimeKai();
+  console.log("Fetching anime info for nippon-sangoku...");
   try {
-    const res = await hianime.getEpisodeSources("nippon-sangoku-rkkm9?ep=1");
-    console.log("HiAnime Sources:", res);
+    const info = await animekai.fetchAnimeInfo("nippon-sangoku-rkkm9");
+    console.log("Got info, fetching sources for ep 1...");
+    if (info.episodes && info.episodes.length > 0) {
+      const epId = info.episodes[0].id;
+      console.log("epId:", epId);
+      const sources = await animekai.fetchEpisodeSources(epId);
+      console.log("Sources:", sources);
+    } else {
+      console.log("No episodes found.");
+    }
   } catch (e) {
-    console.error("HiAnime Error:", e);
+    console.error("Error:", e);
   }
 }
 main();
